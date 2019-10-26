def15787d5cb6b8c88d2a62f7d0a127b6a3c5657
sadj123 <56237377+sadj123@users.noreply.github.com>
2019-10-26 16:39:29 -0500

    Update FlashcardModule.py

--- FlashcardModule.py
+++ FlashcardModule.py
@@ -12,8 +12,17 @@ def Flashcard_Review(file_name):
  
     
     for i in lines:
+        
         (Q,A)=i.split(":")
-        q_and_a[Q] = A
+        p= A.split(",")
+        z= len(p)
+        k= p[z-1]
+        k= k.strip()
+        p.pop()
+        p.append(k)
+        q_and_a[Q] = p
+        
+            
         
 
     while count < len(lines):    
@@ -21,18 +30,18 @@ def Flashcard_Review(file_name):
         if pick not in lista:
             print("\nPregunta", count+1, ": " + "¿" + str(pick) + "?")
             user_answer = str(input("Respuesta: "))
-            answer = str(q_and_a[pick]).strip()
+            answer = q_and_a[pick]
             user_answer = user_answer.lower()
             
-            if user_answer == answer:
+            if user_answer in answer:
                 print("¡Muy bien! Esa es la respuesta correcta ")
                 lista.append(pick)
                 count += 1
                 count_right += 1
-                
+            
             elif user_answer == "":
                 break
-    
+
             else:
                 print("La respuesta es incorrecta. La respuesta correcta es:", answer)
                 lista.append(pick)
@@ -40,4 +49,3 @@ def Flashcard_Review(file_name):
                 count_wrong += 1
                 
     print("\nTus resultados son los siguientes:\n\n Respuestas correctas: {0}\n Respuestas incorrectas: {1}\n Puntuación: {2}/{3}\n Porcentaje: {4}% de las preguntas bien".format(count_right, count_wrong, count_right,count,int((count_right/count)*100)))
-  
