3493756358666519bf8bb083a113a6c31d09419e
SantiagoAlvarezb <56238062+SantiagoAlvarezb@users.noreply.github.com>
2019-11-04 21:24:34 -0500

    Update FlashcardModule.py

--- FlashcardModule.py
+++ FlashcardModule.py
@@ -1,32 +1,32 @@
-import random 
+import random #Se importa la librearia Random ya que la usaremos a lo largo de nuestro modulo y por ende el codigo
 
-def Flashcard_Review(file_name):
-    file = open(str(file_name) + ".txt")
-    lines = file.readlines()
+def Flashcard_Review(file_name): #Se define la funcion llamada "Flashcard_Review" que después será incovacada en el master code. Lo que se será ingresado en la funcion es el nombre del archivo  
+    file = open(str(file_name) + ".txt") #Ya teniendo el nombre del codigo, el programa habrerá el archivo
+    lines = file.readlines() #El codigo lee el archivo linea por linea 
 
-    q_and_a = {}
-    lista=[]
-    count = 0
-    count_right = 0
-    count_wrong = 0
+    q_and_a = {} #Se crea un diccionario vacio asignado a la variable "q_and_a"
+    lista=[] #Se crea una lista vacia asignada a la variable "lista"
+    count = 0 #Se crea un contador que va incrementando cada vez que el codigo pasa por el while loop en la linea 24
+    count_right = 0 #Se crea un contador que incrementa cada vez que el usuario acerta la pregunta
+    count_wrong = 0 #Se crea un contador que incrementa cada vez que el usuraio tenga una pregunta mala
 
-    for i in lines:
-        (Q,A)=i.split(":")
-        p= A.split(";")
-        z= len(p)
+    for i in lines: #Por cada linea que se encuentre en el archivo, el codigo ejecutará este ciclo for
+        (Q,A)=i.split(":") #Dentro del archivo, la pregunta y la respuesta estan separadas por ":" entonces el metodo split logra separarlo
+        p= A.split(";") #El codigo tiene la opcion de que la pregunta tiene mas de una respuesta entonces las otras respuestas esta separadas por ";" entonces el metodo split cumple la funcion de separlas 
+        z= len(p) 
         k= p[z-1]
         k= k.strip()
         p.pop()
         p.append(k)
-        q_and_a[Q] = p
+        q_and_a[Q] = p # Aqui las preguntas y sus respuestas van siendo agregadas al diccionario creado en la linea 7. La pregutnas se vuelve la llave y la respuesta el valor 
 
     print("\nRecuerde que si quiere terminar, presiones ENTER. Al hacerlo, la respuesta automaticamente será incorrecta...")
-    while count < len(lines):    
-        pick = random.choice(list(q_and_a.keys()))
-        if pick not in lista:
+    while count < len(lines): #Como count = 0, el codigo entra dentro de este ciclo while. Solo saldra de este ciclo cuando count iguala al numero de lineas     
+        pick = random.choice(list(q_and_a.keys())) #El random.choice elige una valor dentro de la lista de las llaves del diccionarios. Por ende escoge una pregunta al azar
+        if pick not in lista: #Para prevenir que la misma pregunta se escogida al azar, las preguntas se van agregando a una lsita. El codigo va ejecutar los siguente siempre y cuando la pregunta no se repita
             print("\nPregunta", count+1,": " + "¿" + str(pick) + "?")
-            user_answer = str(input("Respuesta: "))
-            answer = q_and_a[pick]
+            user_answer = str(input("Respuesta: ")) #La respuesta que escriba el usuario se salva dentro de la variable "user_answer" 
+            answer = q_and_a[pick] #La respuestase encuentra dentro de es 
             answer = answer[0]
             user_answer = user_answer.lower()            
 
